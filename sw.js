b248a9c6354ac78483c1711ad5c183d0999cf207
chenryy2581-png <chenryy2581@gmail.com>
2025-10-02 03:03:18 +0800

    service-worker.js

--- sw.js
+++ sw.js
@@ -1,19 +1,30 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = "cargo-system-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./main.js"
+];
+
+// 安裝
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("cargo-cache-v3").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+// 啟用
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
+
+// 請求攔截
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
